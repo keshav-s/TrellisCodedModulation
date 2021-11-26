4323a715c3f6941c1676e39b7e4e80bc6d6a6f71
Keshav Sangam <keshavsangam@keshavs-mbp.wifi.local.cmu.edu>
2021-11-26 12:05:16 -0500

    Changed MMSE indexing to be correct, finally achieve <0.01 BER with these params

--- mmse_le.m
+++ mmse_le.m
@@ -1,8 +1,10 @@
 function vk = mmse_le(zk, mu_init)
     params;
     pk = pilotSymbs;
-    wk = zeros(mmse_len,1); % Initialize LMS algorithm
-    
+    pilotsymbolindex = timingT+1;
+    Trainlen = pilotT;
+    wk = zeros(w_len,1); % Initialize LMS algorithm
+
     vk = [];
     for i=1:num_packets
         if i==1
@@ -11,27 +13,31 @@ function vk = mmse_le(zk, mu_init)
             zk_packet = zk(packetLen*(i-1)+1:end);
         end
 
-        mu = gammaMMSE/mean(abs(zk(timingT+1:timingT+pilotT)).^2); % Normalized step-size, normalized by filter input energy
-
+        mu = gamma_f/mean(abs(zk).^2); % Normalized step-size, normalized by filter input energy
         % Training phase of LMS algorithm for Practical MMSE-LE
         for mm=1:num_train_epochs
-            for (ii=timingT+1:timingT+pilotT-1)
-               zk_past = zk_packet(ii+le_delay:-1:ii+le_delay-mmse_len+1);
-               vv(ii,1) = transpose(wk)*zk_past; % LE Filter output
-               ek(ii,1) = vv(ii) - pk(ii-timingT+1); % ek = vk - xk
-               wk = wk - mu*ek(ii,1)*conj(zk_past); % Adapt filter. Use normalized stepsize
+            for (ii=pilotsymbolindex:pilotsymbolindex+Trainlen-1)
+               zz_past = zk_packet(ii+f_delay:-1:ii+f_delay-w_len+1);
+               vv(ii,1) = transpose(wk)*zz_past; % LE Filter output
+               ee(ii,1) = vv(ii) - pk(ii-timingT)*1.25; % ek = vk - pk
+               wk = wk - mu*ee(ii)*conj(zz_past); % Adapt filter. Use normalized stepsize
             end
             mu = mu*mu_scaling; % Change the mu for next epoch
         end
         
         % Data phase for Practical MMSE-LE
         j = 1;
-        for (ii=timingT+pilotT:length(zk_packet)-le_delay)
-           zk_past = zk_packet(ii+le_delay:-1:ii+le_delay-mmse_len+1); 
-           vv(j,1) = transpose(wk)*zk_past; % Output of Practical MMSE-LE filter
+        for (ii=pilotsymbolindex+Trainlen:length(zk_packet)-f_delay)
+           zz_past = zk_packet(ii+f_delay:-1:ii+f_delay-w_len+1); 
+           vv(j,1) = transpose(wk)*zz_past; % Output of Practical MMSE-LE filter
            j=j+1;
         end
-        vk = [vk; vv(1:packetT,1)];
-        wk = zeros(mmse_len,1); % Initialize LMS algorithm
+
+        if i == num_packets && mod(messageLen, packetT) > 0 
+            vk = [vk; vv(1:mod(messageLen, packetT))];
+        else
+            vk = [vk; vv(1:packetT)];
+        end
+        % wk = zeros(w_len,1); % Initialize LMS algorithm
     end
 end
--- params.m
+++ params.m
@@ -13,38 +13,33 @@ use_one_tap = 0;
 B = 4;            % Bits per symbol
 M = 2^B;          % Symbol order
 rolloff = 0.2;    % Rolloff factor for SRRCR pulse
-freqT = 200;      % Number of symbol periods in the frequency preamble
+freqT = 400;      % Number of symbol periods in the frequency preamble
 timingT = 100;    % Number of symbol periods in the timing preamble
 fsamp = 200;      % Sampling frequency in MHz.  DON'T CHANGE
-N = 51;           % Length of filter in symbol periods. Default is 51
+N = 50;           % Length of filter in symbol periods. Default is 51
 messageLen = length(bits)/B;
-if use_one_tap
-    pilotT = 50;      % Number of symbol periods in pilot sequence
-    packetT = 200;    % Number of symbol periods in message between pilot inserts 
-    packetLen = packetT + pilotT;
+
+pilotT = 73;
+packetT = 270;
+packetLen = packetT + pilotT;
+if mod(messageLen,packetT) > 0
     num_packets = 1+floor(messageLen/packetT);
-    LL = freqT + timingT + num_packets*pilotT + messageLen; % Total number of symbols
 else
-    % MMSE LE Parameters
-    mmse_len = 5; % Number of taps in Practical MMSE-LE filter. Try 5
-    gammaMMSE = 0.18; % Step-size normalization constant of LMS algorithm. Try 0.01.  
-    num_train_epochs = 81; % Run over the same pilot sequence repeatedly this many times, to use the pilot more comprehensively
-    mu_scaling = 0.98; % Reduce the mu by this factor each epoch
-    le_delay =  2; % To allow noncausal Practical MMSE-LE filter design, let filter predict a **past** symbol
-
-    pilotT = 75;
-    num_segs = 15;
-    packetT = messageLen/num_segs;    % Number of symbol periods in message between pilot inserts 
-    packetLen = packetT + pilotT;
-    if mod(messageLen,packetT) > 0
-        num_packets = 1+floor(messageLen/packetT);
-    else
-        num_packets = floor(messageLen/packetT);
-    end
-    LL = freqT + timingT + num_packets*pilotT + messageLen; % Total number of symbols
+    num_packets = floor(messageLen/packetT);
 end
+LL = freqT + timingT + num_packets*pilotT + messageLen; % Total number of symbols
 
-fs = 12;          % Over-sampling factor (Sampling frequency/symbol rate). Choose 20, so that 
+% MMSE Parameters
+w_len = 7; % Number of taps in Practical MMSE-LE filter. Try 5
+b_len  = 5;
+gamma_f = 0.08; % Step-size normalization constant of LMS algorithm. Try 0.01.  
+gamma_b = 0.08; % Step-size normalization constant of LMS algorithm. Try 0.01.  
+num_train_epochs = 10; % Run over the same pilot sequence repeatedly this many times, to use the pilot more comprehensively
+mu_scaling = 0.98; % Reduce the mu by this factor each epoch
+f_delay =  3; % let filter predict a **past** symbol for practical feedforward adaptive filtering
+
+
+fs = 11;          % Over-sampling factor (Sampling frequency/symbol rate). Choose 20, so that 
                   % symbol rate = sampling frequency/fs = 200/14 = 10 MHz.
                   % fs is also the number of samples that make up one symbol period
 T = 1/(fsamp/fs); % Sampling period in microseconds
